b8b08f09d82c58cb62591723d37c3b0799e30c34
Kameshraj05 <131840872+Kameshraj05@users.noreply.github.com>
2025-04-21 11:43:21 +0530

    Delete src directory

--- src/main/java/com/assignment/rewardsapi/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.rewardsapi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.assignment.rewardsapi.model.Customer;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-	
-	Customer findByCustomerId(String customerId);
-}
